6d5c823efc7986341a4cc7d81dcc87a6d705a49d
eelabnctu <37565992+eelabnctu@users.noreply.github.com>
2018-03-20 11:59:39 +0800

    Create freq_div_8.v

---
+++ Frequency_Divider/freq_div_8.v
@@ -0,0 +1,18 @@
+// example for divisor is equal to 2-order
+module my_freq_div(
+    input clk,
+    output div_out_2,
+    output div_out_8
+    );
+    reg [15:0]counter = 0;
+    
+    always@(posedge clk)
+        counter <= counter + 1;
+        
+    // board clock freq / 2^(0+1)
+    assign div_out_2 = counter[0];
+    
+    // board clock freq / 2^(2+1)
+    assign div_out_8 = counter[2];
+       
+endmodule
