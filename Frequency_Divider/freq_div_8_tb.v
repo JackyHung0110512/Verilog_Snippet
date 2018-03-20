07c4d8bee18a3839a6a38ed25b52b879b4016369
eelabnctu <37565992+eelabnctu@users.noreply.github.com>
2018-03-20 12:00:37 +0800

    Create freq_div_8_tb.v

---
+++ Frequency_Divider/freq_div_8_tb.v
@@ -0,0 +1,15 @@
+module my_freq_div_tb();
+    reg clk;
+    
+    wire div_2;
+    wire div_8;
+    
+    my_freq_div UUT(clk, div_2, div_8);
+    
+    initial
+        clk = 0;
+    
+    always
+        #5 clk = ~clk;
+        
+endmodule
