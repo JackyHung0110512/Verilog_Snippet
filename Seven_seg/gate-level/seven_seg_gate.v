550625aec63e4745f2b2abdc99daa3d21f2feafb
NCTU Electrical & Electronics Laboratory <37565992+eelabnctu@users.noreply.github.com>
2018-03-26 11:25:33 +0800

    Create seven_seg_gate.v

---
+++ Seven_seg/gate-level/seven_seg_gate.v
@@ -0,0 +1,27 @@
+module my_7_segment(
+	input [3:0] q,
+	input [7:0] an_in,
+	input dp_sw,
+	output a,
+	output b,
+	output c,
+	output d,
+	output e,
+	output f,
+	output g,
+	output dp,
+	output [7:0] an
+    );
+
+	assign a = (~q[3] & ~q[2] & ~q[1] & q[0]) | (q[2] & ~q[0]) | (q[3] & q[1]);
+	assign b = (q[2] & ~q[1] & q[0]) | (q[2] & q[1] & ~q[0]) | (q[3] & q[1]);
+	assign c = (~q[2] & q[1] & ~q[0]) | (q[3] & q[2]);
+	assign d = (~q[2] & ~q[1] & q[0]) | (q[2] & ~q[1] & ~q[0]) | (q[2] & q[1] & q[0]);
+	assign e = q[0] + (q[2] & ~q[1]);
+	assign f = (~q[3] & ~q[2] & q[0]) | (~q[2] & q[1]) | (q[1] & q[0]);
+	assign g = (~q[3] & ~q[2] & ~q[1]) | (q[2] & q[1] & q[0]);
+	assign dp = ~dp_sw;
+	assign an = ~an_in;
+		
+
+endmodule
